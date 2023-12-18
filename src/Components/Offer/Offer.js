4d9445d5d0175a2c81713cea59aae144831e1b62
HarshilKhimasia <harshilkhimasia22@gmail.com>
2023-12-18 10:24:13 +0530

    Created menu and first section

---
+++ src/Components/Offer/Offer.js
@@ -0,0 +1,28 @@
+import "./Offer.scss";
+
+function Offer() {
+  return (
+    <div className="offer">
+      <span>
+        Save up to ₹8000.00 instantly on eligible products with HDFC Bank Credit
+        Cards.
+        <sup>
+          <a href="#">*&nbsp;</a>
+        </sup>
+        Plus No Cost EMI from most leading banks.
+        <sup>
+          <a href="#">‡&nbsp;</a>
+        </sup>
+        <a href="#">
+          Shop now
+          <img
+            src="/assets/chevron-right.svg"
+            alt="chevron-right"
+            width={"16px"}
+          />
+        </a>
+      </span>
+    </div>
+  );
+}
+export default Offer;
