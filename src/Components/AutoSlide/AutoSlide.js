14ecab61b49da2b70977b3e0e2878c95bdb65c29
HarshilKhimasia <harshilkhimasia22@gmail.com>
2023-12-19 16:32:14 +0530

    1920 version completed

--- src/Components/AutoSlide/AutoSlide.js
+++ src/Components/AutoSlide/AutoSlide.js
@@ -7,6 +7,8 @@ function AutoSlide() {
   const options = {
     type: "loop",
     gap: "1rem",
+    speed: 1000,
+    easing: "cubic-bezier(.42,0,.58,1) ",
     autoplay: true,
     pauseOnHover: false,
     resetProgress: false,
@@ -15,28 +17,43 @@ function AutoSlide() {
 
   return (
     <div className="wrapper">
-      <h2 id="autoplay-example-heading">Autoplay</h2>
       <Splide
-        options={options}
+        options={{
+          ...options, // Keep your existing options
+          perPage: 1.5,
+          focus: "center",
+          arrows: false,
+          // Add any other options you may have here
+        }}
         aria-labelledby="autoplay-example-heading"
         hasTrack={false}
       >
         <div style={{ position: "relative" }}>
           <SplideTrack>
             <SplideSlide>
-              <img src="./assets/slide-1.jpg" alt="slide-1" />
+              <div className="cistome-slide">
+                <img src="./assets/slide-1.jpg" alt="slide-1" />
+              </div>
             </SplideSlide>
             <SplideSlide>
-              <img src="./assets/slide-2.jpg" alt="slide-2" />
+              <div className="cistome-slide">
+                <img src="./assets/slide-2.jpg" alt="slide-2" />
+              </div>
             </SplideSlide>
             <SplideSlide>
-              <img src="./assets/slide-3.jpg" alt="slide-3" />
+              <div className="cistome-slide">
+                <img src="./assets/slide-3.jpg" alt="slide-3" />
+              </div>
             </SplideSlide>
             <SplideSlide>
-              <img src="./assets/slide-4.jpg" alt="slide-4" />
+              <div className="cistome-slide">
+                <img src="./assets/slide-4.jpg" alt="slide-4" />
+              </div>
             </SplideSlide>
             <SplideSlide>
-              <img src="./assets/slide-5.jpg" alt="slide-5" />
+              <div className="cistome-slide">
+                <img src="./assets/slide-5.jpg" alt="slide-5" />
+              </div>
             </SplideSlide>
           </SplideTrack>
         </div>
@@ -46,8 +63,21 @@ function AutoSlide() {
         </div>
 
         <button className="splide__toggle">
-          <span className="splide__toggle__play">Play</span>
-          <span className="splide__toggle__pause">Pause</span>
+          <img
+            src="./assets/play-svgrepo-com.svg"
+            className="splide__toggle__play"
+            alt="play-svgrepo-com"
+            width={"25px"}
+            height={"25px"}
+          />
+
+          <img
+            src="./assets/pause-circle-svgrepo-com.svg"
+            className="splide__toggle__pause"
+            alt="pause-circle-svgrepo-com"
+            width={"25px"}
+            height={"25px"}
+          />
         </button>
       </Splide>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,70 +1,152 @@
+import { useEffect, useRef, useState } from "react";
 import "./Navbar.scss";
 function Navbar() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isLinkHovered, setIsLinkHovered] = useState(false);
+  const navRef = useRef(null);
+  const collapsedCurtainRef = useRef(null);
+  const menuGridRef = useRef(null);
+  const [navHeight, setNavHeight] = useState(0);
+
+  useEffect(() => {
+    if (navRef.current) {
+      setNavHeight(navRef.current.clientHeight);
+    }
+  }, [navRef]);
+  const getMenuGridHeight = () => {
+    if (menuGridRef.current) {
+      return (
+        menuGridRef.current.clientHeight +
+        0.5 * menuGridRef.current.clientHeight
+      );
+    }
+    return 0;
+  };
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="https://www.apple.com/in/">
-            <img
-              src="/assets/logo-one.svg"
-              height={"auto"}
-              width={"16px"}
-              alt="logo"
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">Store</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">Mac</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">iPad</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">iPhone</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">Watch</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">AirPods</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">TV & Home</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">Entertainment</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">Accessories</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">Support</a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">
-            <img
-              src="/assets/search-icon.svg"
-              height={"auto"}
-              width={"17px"}
-              alt="search-icon"
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.apple.com/in/">
-            <img
-              src="/assets/bag-icon.svg"
-              height={"auto"}
-              width={"17px"}
-              alt="search-icon"
-            />
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav ref={navRef} className={isHovered ? "hovered" : ""}>
+        <ul>
+          <li>
+            <a href="https://www.apple.com/in/">
+              <img
+                src="/assets/logo-one.svg"
+                height={"auto"}
+                width={"16px"}
+                alt="logo"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.apple.com/in/"
+              onMouseEnter={() => setIsHovered(true)}
+            >
+              Store
+            </a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">Mac</a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">iPad</a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">iPhone</a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">Watch</a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">AirPods</a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">TV & Home</a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">Entertainment</a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">Accessories</a>
+          </li>
+          <li onMouseEnter={() => setIsHovered(true)}>
+            <a href="https://www.apple.com/in/">Support</a>
+          </li>
+          <li>
+            <a href="https://www.apple.com/in/">
+              <img
+                src="/assets/search-icon.svg"
+                height={"auto"}
+                width={"17px"}
+                alt="search-icon"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.apple.com/in/">
+              <img
+                src="/assets/bag-icon.svg"
+                height={"auto"}
+                width={"17px"}
+                alt="search-icon"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div
+        ref={collapsedCurtainRef}
+        className={`collapsed-curtain ${isHovered ? "expanded" : ""}`}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div ref={menuGridRef} className="menuGrid">
+          <div className="menu-col">
+            <h6>Shop</h6>
+            <ul className="big-list">
+              <li>Shop the Latest</li>
+              <li>Mac</li>
+              <li>iPad</li>
+              <li>iPhone</li>
+              <li>Apple Watch</li>
+              <li>Accessories</li>
+            </ul>
+          </div>
+          <div className="menu-col">
+            <h6>Quick Links</h6>
+            <ul className="small-list">
+              <li>Shop Holiday Gifts</li>
+              <li>Find a Store</li>
+              <li>Order Status</li>
+              <li>Ways to Buy</li>
+            </ul>
+          </div>
+          <div className="menu-col">
+            <h6>Shop Special Stores</h6>
+            <ul className="small-list">
+              <li>Education</li>
+              <li>Business</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className={`behind-overlay ${isHovered ? "gos-on" : ""}`}></div>
+      <style jsx>{`
+        .collapsed-curtain {
+          top: ${navHeight}px;
+          left: 0;
+        }
+
+        .collapsed-curtain.expanded {
+          height: ${getMenuGridHeight()}px;
+        }
+
+        .behind-overlay.gos-on {
+          opacity: 1;
+          height: 80%;
+          transition: opacity 0.5s ease-in-out;
+        }
+      `}</style>
+    </>
   );
 }
 
