ac822c46ad5e479134321e6cbc2f9c851df7cd0c
HarshilKhimasia <harshilkhimasia22@gmail.com>
2023-12-20 00:02:58 +0530

    Final Project Commit

--- src/Components/AutoSlide/AutoSlide.js
+++ src/Components/AutoSlide/AutoSlide.js
@@ -22,7 +22,7 @@ function AutoSlide() {
           ...options, // Keep your existing options
           perPage: 1.5,
           focus: "center",
-          arrows: false,
+          arrows: true,
           // Add any other options you may have here
         }}
         aria-labelledby="autoplay-example-heading"
@@ -32,27 +32,367 @@ function AutoSlide() {
           <SplideTrack>
             <SplideSlide>
               <div className="cistome-slide">
-                <img src="./assets/slide-1.jpg" alt="slide-1" />
+                <img
+                  src="./assets/slide-1.jpg"
+                  alt="slide-1"
+                  className="lg-slide"
+                />
+                <img
+                  src="./assets/slide-1-mob.jpg"
+                  alt="slide-1-mob"
+                  className="sm-slide"
+                />
+                <div class="inner">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                    <p class="typography-shows-genre">
+                      <span class="genre">Drama</span>
+                      <span class="m-dot" aria-hidden="true">
+                        &nbsp;·&nbsp;
+                      </span>
+                      Change is on the air.
+                    </p>
+                  </div>
+                </div>
+                <div class="outer">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <p class="typography-shows-genre">
+                      <span class="genre">
+                        Drama<br></br>
+                      </span>
+                      Change is on the air.
+                    </p>
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                  </div>
+                </div>
               </div>
             </SplideSlide>
             <SplideSlide>
               <div className="cistome-slide">
-                <img src="./assets/slide-2.jpg" alt="slide-2" />
+                <img
+                  src="./assets/slide-2.jpg"
+                  alt="slide-2"
+                  className="lg-slide"
+                />
+                <img
+                  src="./assets/slide-2-mob.jpg"
+                  alt="slide-2-mob"
+                  className="sm-slide"
+                />
+                <div class="inner">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                    <p class="typography-shows-genre">
+                      <span class="genre">Drama</span>
+                      <span class="m-dot" aria-hidden="true">
+                        &nbsp;·&nbsp;
+                      </span>
+                      Change is on the air.
+                    </p>
+                  </div>
+                </div>
+                <div class="outer">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <p class="typography-shows-genre">
+                      <span class="genre">
+                        Drama<br></br>
+                      </span>
+                      Change is on the air.
+                    </p>
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                  </div>
+                </div>
               </div>
             </SplideSlide>
             <SplideSlide>
               <div className="cistome-slide">
-                <img src="./assets/slide-3.jpg" alt="slide-3" />
+                <img
+                  src="./assets/slide-3.jpg"
+                  alt="slide-3"
+                  className="lg-slide"
+                />
+                <img
+                  src="./assets/slide-3-mob.jpg"
+                  alt="slide-3-mob"
+                  className="sm-slide"
+                />
+                <div class="inner">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                    <p class="typography-shows-genre">
+                      <span class="genre">Drama</span>
+                      <span class="m-dot" aria-hidden="true">
+                        &nbsp;·&nbsp;
+                      </span>
+                      Change is on the air.
+                    </p>
+                  </div>
+                </div>
+                <div class="outer">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <p class="typography-shows-genre">
+                      <span class="genre">
+                        Drama<br></br>
+                      </span>
+                      Change is on the air.
+                    </p>
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                  </div>
+                </div>
               </div>
             </SplideSlide>
             <SplideSlide>
               <div className="cistome-slide">
-                <img src="./assets/slide-4.jpg" alt="slide-4" />
+                <img
+                  src="./assets/slide-4.jpg"
+                  alt="slide-4"
+                  className="lg-slide"
+                />
+                <img
+                  src="./assets/slide-4-mob.jpg"
+                  alt="slide-4-mob"
+                  className="sm-slide"
+                />
+                <div class="inner">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                    <p class="typography-shows-genre">
+                      <span class="genre">Drama</span>
+                      <span class="m-dot" aria-hidden="true">
+                        &nbsp;·&nbsp;
+                      </span>
+                      Change is on the air.
+                    </p>
+                  </div>
+                </div>
+                <div class="outer">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <p class="typography-shows-genre">
+                      <span class="genre">
+                        Drama<br></br>
+                      </span>
+                      Change is on the air.
+                    </p>
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                  </div>
+                </div>
               </div>
             </SplideSlide>
             <SplideSlide>
               <div className="cistome-slide">
-                <img src="./assets/slide-5.jpg" alt="slide-5" />
+                <img
+                  src="./assets/slide-5.jpg"
+                  alt="slide-5"
+                  className="lg-slide"
+                />
+                <img
+                  src="./assets/slide-5-mob.jpg"
+                  alt="slide-5-mob"
+                  className="sm-slide"
+                />
+                <div class="inner">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                    <p class="typography-shows-genre">
+                      <span class="genre">Drama</span>
+                      <span class="m-dot" aria-hidden="true">
+                        &nbsp; · &nbsp;
+                      </span>
+                      Change is on the air.
+                    </p>
+                  </div>
+                </div>
+                <div class="outer">
+                  <div class="info-top">
+                    {/* <figure class="atv-plus-icon"></figure>
+                    <figure
+                      class="logo"
+                      style="background-image: url(https://is1-ssl.mzstatic.com/image/thumb/qOsAe0YuSE0kbguTATsVLQ/548x165.png)"
+                    ></figure> */}
+                  </div>
+                  <div class="info-bottom">
+                    <p class="typography-shows-genre">
+                      <span class="genre">
+                        Drama<br></br>
+                      </span>
+                      Change is on the air.
+                    </p>
+                    <div
+                      class="custom-button play"
+                      aria-label="Stream now, The Morning Show"
+                    >
+                      Stream now
+                      <figure>
+                        <img
+                          src="../assets/play-circle-svgrepo-com.svg"
+                          alt="play-circle-svgrepo-com"
+                        />
+                      </figure>
+                    </div>
+                  </div>
+                </div>
               </div>
             </SplideSlide>
           </SplideTrack>
